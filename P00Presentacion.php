dc2faf45576a9f9cc056b8b0b40d18722b8ab3a2
TEORYR19 <lopez.esquivel.and@gmail.com>
2024-10-21 09:29:49 -0600

    Add files via upload

---
+++ P00Presentacion.php
@@ -0,0 +1,13 @@
+<?php
+/*CBTIS 89
+P200Presentacion.php
+programa que contitene mis datos personales
+Jesus Andres Lopez Esquivel
+3°A Programacion Matutino*/
+
+echo "NOMBRE: Jesus Andres Lopez Esquivel <br>";
+echo "ESCUELA: CBTIS 89 <BR>";
+echo "EDAD: 16 años <br>";
+echo "COLONIA: Nuevo Durango 2 <br>";
+echo "CIUDAD, PAIS: Durango Mexico <br>";
+?>
