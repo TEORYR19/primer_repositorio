0fc49165da5244402bb881114cdd3c66f05d48e9
TEORYR19 <lopez.esquivel.and@gmail.com>
2024-11-20 10:21:57 -0600

    Add files via upload

---
+++ Unidad3 Lopez Esquivel/Examen3LopezEsquivel.php
@@ -0,0 +1,19 @@
+<?php
+/*programa que almaena el nombrede 4 personas*/
+$Alumnos = array("Luz, Fer, Ian, Ale");
+$Califiaiones = array(6, 4, 5, 7, 9, 8, 7, 9, 5, 5, 4, 6);
+$Status = array(if ($pf>=6)
+echo "El alumno aprobo";
+
+else 
+echo "El alumno reprobo";)
+$Promedio = raray();
+
+
+
+
+echo "Alumnos ----- Califiaiones ----- Status ----- Promedio<br>";
+for ($k = 0; $k < count($Alumnos); $k++) {
+    echo $Alumnos[$k] . " ------------- " . $Califiaiones[$k] . " ------------ " . $Status[$k] . " ------------ " . $Promedio[$k] . "<br>";
+}
+?>
